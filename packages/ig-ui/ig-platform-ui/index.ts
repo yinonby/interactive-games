cfebbab4e92e2e817ef7b823e427d97d619910bf
Yinon-mac <yinonby@yahoo.com>
2026-01-18 02:44:48 +0100

    add PlatformUiLocalization

--- packages/ig-ui/ig-platform-ui/index.ts
+++ packages/ig-ui/ig-platform-ui/index.ts
@@ -1,5 +1,6 @@
 
 export * from "./src/components/PlatformUiLink";
+export * from "./src/localization/PlatformUiLocalization";
 export * from "./src/navigation/PlatformUiNavigationProvider";
 export * from "./src/platform/PlatformUtils";
 export * from "./src/storage/Storage";
