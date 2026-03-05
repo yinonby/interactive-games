9b1e490f9f785fd1fee25c45471b504770f91f96
Yinon-mac <yinonby@yahoo.com>
2026-03-05 07:34:01 +0100

    rename curAccoutId to curAuthId in auth package

--- packages/ig-games/ig-games-engine/ig-games-engine-ui/src/features/game-instance/dashboard/components/PlayersTableView.tsx
+++ packages/ig-games/ig-games-engine/ig-games-engine-ui/src/features/game-instance/dashboard/components/PlayersTableView.tsx
@@ -1,7 +1,7 @@
 
 import { useAppLocalization, useClientLogger } from '@ig/app-engine-ui';
 import { useAuth } from '@ig/auth-ui';
-import { comparePlayersForDisplaySort, type PublicPlayerInfoT } from '@ig/games-engine-models';
+import { comparePlayersForDisplaySort, type GameUserIdT, type PublicPlayerInfoT } from '@ig/games-engine-models';
 import { RnuiTable, RnuiTableHeader, RnuiTableTitle, RnuiText } from '@ig/rnui';
 import React, { type FC } from 'react';
 import { View } from 'react-native';
@@ -17,16 +17,17 @@ export const PlayersTableView: FC<PlayersTableViewPropsT> = (props) => {
   const { publicPlayerInfos, withAdminButtons } = props;
   const { t } = useAppLocalization();
   const logger = useClientLogger();
-  const { curAccountId } = useAuth();
+  const { curAuthId } = useAuth();
+  const curGameUserId = curAuthId as GameUserIdT;
 
-  const curPublicPlayerInfo = publicPlayerInfos.find(e => e.playerId === curAccountId);
+  const curPublicPlayerInfo = publicPlayerInfos.find(e => e.playerId === curGameUserId);
   if (curPublicPlayerInfo === undefined) {
-    logger.error(`Unexpected game instance not belonging to player, curAccountId [${curAccountId}]`);
+    logger.error(`Unexpected game instance not belonging to player, curGameUserId [${curGameUserId}]`);
     return null;
   }
   const isCurUserAdminPlayer = curPublicPlayerInfo.playerRole === 'admin';
   const otherPublicPlayerInfos = publicPlayerInfos
-    .filter(e => e.playerId !== curAccountId)
+    .filter(e => e.playerId !== curGameUserId)
     .sort(comparePlayersForDisplaySort);
 
   return (
