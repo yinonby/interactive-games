ada3e3a50a97bc9df0f1b714b4162495e50fc0dd
Yinon-mac <yinonby@yahoo.com>
2026-03-05 11:59:50 +0100

    remove unused interface

--- packages/ig-auth-plugin/ig-auth-api/src/types/AuthPluginTypes.ts
+++ packages/ig-auth-plugin/ig-auth-api/src/types/AuthPluginTypes.ts
@@ -1,16 +1,11 @@
 
 import type { SignupPluginAdapter, SignupServiceTransactionAdapter } from '@ig/auth-be-models';
 import type { AuthIdT, UserT } from '@ig/auth-models';
-import type { Request, Response } from 'express';
+import type { Response } from 'express';
 
 export type AuthPluginConfigT = {
   getSignupServiceTransactionAdapter: () => SignupServiceTransactionAdapter,
   getSignupPluginAdapter: () => SignupPluginAdapter,
-}
-
-export interface ApolloContextT {
-  req: Request;
-  res: Response;
 }
 
 export interface SignupServiceAdapter {
